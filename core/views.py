a8888aec639dd45854cc70a6e4cca9cc9bf02fb8
alecsandros <ascj@cin.ufpe.br>
2019-11-13 09:42:48 -0300

    Funcionalidade de email

--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import EmailMessage
 
 
 def index(request):
     return render(request, 'home.html')
+
+from django.core.mail import EmailMessage
+
+def email(request):
+    print(request)
+    subject = ''
+    body = ''
+    body += ''
+
+    mail = EmailMessage(subject, body, 'teste.pta.citi@gmail.com', ['teste.pta.citi@gmail.com'])
+    result = mail.send()
+
+    if (result):
+            return HttpResponse("success")
+    return HttpResponse("error")
