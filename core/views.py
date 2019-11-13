29cea4192463362e65d091ba4b8aaebff64bd37f
alecsandros <ascj@cin.ufpe.br>
2019-11-13 13:50:14 -0300

    Ajustando mensagem final

--- core/views.py
+++ core/views.py
@@ -9,11 +9,16 @@ def index(request):
 from django.core.mail import EmailMessage
 
 def email(request):
-    print(request)
-    subject = ''
-    body = ''
-    body += ''
+    name = request.POST.get('name')
+    mail = request.POST.get('mail')
+    subject = request.POST.get('subject')
+    message = request.POST.get('message')
 
+    body = 'Mensagem de: %s (%s) \n'%(name, mail)
+    body += 'Assunto: %s \nMensagem: %s '%(subject, message)
+
+    subject = '[Contato] %s'%(subject)
+    
     mail = EmailMessage(subject, body, 'teste.pta.citi@gmail.com', ['teste.pta.citi@gmail.com'])
     result = mail.send()
 
